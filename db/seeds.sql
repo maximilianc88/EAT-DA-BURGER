e80117c901f79f3f17f4dcc32d4b8933dbfe4905
Maximillian Cartwright <maximilianc@zillowgroup.com>
2020-02-29 16:28:34 -0800

    updated schemas

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burger (name) VALUES ('Meatball sandwich');
+INSERT INTO burger (name) VALUES ('Chili sandwich');
+INSERT INTO burger (name) VALUES ('Gyro');
+INSERT INTO burger (name, devoured) VALUES ('Chicken sandwich', true);
+INSERT INTO burger (name, devoured) VALUES ('Turkey club with no bacon', true);

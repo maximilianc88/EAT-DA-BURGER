01ef47d436df43f2c39fc3c7db73eb828c15a41c
Maximillian Cartwright <maximilianc@zillowgroup.com>
2020-05-11 16:04:38 -0700

    deploying to heroku with jawsDB and some minor updates

--- configuration/connection.js
+++ configuration/connection.js
@@ -2,22 +2,21 @@
 
 // Set up MySQL connection.
 const mysql = require('mysql');
+let connection;
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  password: 'penguin',
-  database: 'burger_db',
-});
+if (process.env.JawsDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'penguin',
+    database: 'burger_db',
+  });
+};
 
 
-connection.connect(err => {
-    if (err) {
-        console.error('error connecting', err.stack);
-        return;
-    }
-    console.log(`connected with id ${connection.threadId}`);
-});
+connection.connect();
 
 module.exports = connection;
